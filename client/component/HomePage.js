99cec327dfa0307b9a195c4681993b7a2eed3a8a
namrata25 <ngup75@sapient.com>
2016-11-30 17:37:30 +0530

    login page completed

--- client/component/HomePage.js
+++ client/component/HomePage.js
@@ -6,10 +6,6 @@ import Header from './Header'
 class HomePage extends Component {
   componentWillMount() {
     this.props.fetchTypes();
-    setTimeout(() => {
-     window.history.forward()
-   }, 0)
-   window.onunload=function(){null};
   }
   render() {
     const typeList = this.props.types[0].types;
@@ -27,5 +23,24 @@ class HomePage extends Component {
     )
   }
 }
+
+window.addEventListener('popstate', function(event) {
+  // The popstate event is fired each time when the current history entry changes.
+
+  var r = confirm("You pressed a Back button! Are you sure?!");
+
+  if (r == true) {
+      // Call Back button programmatically as per user confirmation.
+      history.back();
+      // Uncomment below line to redirect to the previous page instead.
+      // window.location = document.referrer // Note: IE11 is not supporting this.
+  } else {
+      // Stay on the current page.
+      history.pushState(null, null, window.location.pathname);
+  }
+
+  history.pushState(null, null, window.location.pathname);
+
+}, false);
 
 export default HomePage;
